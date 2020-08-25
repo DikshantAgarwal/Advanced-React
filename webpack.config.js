8f0dc8e76ba9a4ee798dcfe9f30a8e4d0d3739fa
unknown <dikshantagarwal234@gmail.com>
2020-08-25 12:36:27 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+
+  resolve:{
+    modules:[
+      path.resolve('./lib'),
+      path.resolve('./node_modules')
+    ]
+  },
+
+  entry:['./lib/renderer/dom.js'],
+  output: {
+    path:path.resolve(__dirname,'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/,exclude:/node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
